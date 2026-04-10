0d29f9d33e829f187a7b158fbf5c257d0bb1b9a0
Dimitri Fiacre <dimitri.fcr@gmail.com>
2026-04-10 14:32:10 +0200

    feat(auth): add login page

--- src/components/ui/checkbox.tsx
+++ src/components/ui/checkbox.tsx
@@ -14,7 +14,7 @@ function Checkbox({
     <CheckboxPrimitive.Root
       data-slot="checkbox"
       className={cn(
-        "border-input bg-input enabled:hover:bg-input-hover enabled:hover:border-border-hover data-[state=checked]:bg-primary data-[state=checked]:border-none data-checked:bg-primary data-checked:border-primary data-[state=checked]:enabled:hover:bg-primary-hover aria-invalid:aria-checked:border-primary aria-invalid:ring-destructive aria-invalid:!border-destructive focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 focus-visible:ring-offset-background flex size-4 items-center justify-center rounded-sm border transition-colors group-has-disabled/field:opacity-disabled peer relative shrink-0 outline-none after:absolute after:-inset-x-3 after:-inset-y-2 disabled:cursor-not-allowed disabled:opacity-disabled",
+        "border-input bg-input enabled:hover:bg-input-hover enabled:hover:border-border-hover data-[state=checked]:bg-primary data-[state=checked]:border-none data-checked:bg-primary data-checked:border-primary data-[state=checked]:enabled:hover:bg-primary-hover aria-invalid:aria-checked:border-primary aria-invalid:ring-destructive aria-invalid:!border-destructive focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 focus-visible:ring-offset-background flex size-4 items-center justify-center rounded-sm border transition-colors group-has-disabled/field:opacity-disabled peer relative shrink-0 outline-none cursor-pointer after:absolute after:-inset-x-3 after:-inset-y-2 disabled:cursor-not-allowed disabled:opacity-disabled",
         className
       )}
       {...props}
